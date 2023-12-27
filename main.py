b32eb74b79711643eab5e241e2a0cdac73ed7d7a
German <germanzlobin2@gmail.com>
2023-12-27 22:35:11 +0700

    2.0 new data window

--- main.py
+++ main.py
@@ -4,28 +4,26 @@ from PyQt5 import QtWidgets
 import pandas as pd
 from styles.SettingsMenuStyle import Ui_settings
 from styles.MainMenuStyle import Ui_MainWindow
+from styles.DataMenuStyle import Ui_data
 import sys
 
 
-class SettingsWindow(QMainWindow, Ui_settings):
+class DataWinwow(QMainWindow, Ui_data):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         # подключаем кнопки к функциям
-        self.pushButton.pressed.connect(self.apply)
-        self.pushButton_2.pressed.connect(self.close_window)
+
         self.setMinimumSize(500, 400)
         # задаем data, где будет храниться dataframe
         self.data = None
-        # в MainMenu будет передаваться объект класса основного окна для дальнейшей работы
-        self.MainMenu = None
-
-    def close_window(self):
-        self.close()
+        self.pushButton.clicked.connect(self.apply)
+        self.pushButton_2.clicked.connect(self.close)
+        self.SettingsMenu = None
 
     def apply(self):  # функция принять
-        if self.LineEdit.text():  # если есть разделитель, то считываем и обновляем его
-            self.MainMenu.delimiter = self.LineEdit.text()
+        print("ok")
+
 
         # если данных нет, или ничего не выбрано, то выходим из функции
         if self.data.empty or not self.tableWidget.selectedRanges():
@@ -34,20 +32,20 @@ class SettingsWindow(QMainWindow, Ui_settings):
         # создаем новый dataframe с выбранными значениями
         book = {}  # словарь для значений
         headers = self.MainMenu.data.columns  # список заголовков
-        # values = self.MainMenu.data.values.tolist()  # вложенный список всех значений
+        values = self.MainMenu.data.values.tolist()  # вложенный список всех значений
 
-        # for selected in self.tableWidget.selectedRanges():  # бежим по выбранным значениям и добавляем их в book
-        #     for index in range(selected.leftColumn(), selected.rightColumn() + 1, 1):
-        #         book[headers[index]] = [values[j][index] for j in range(selected.topRow(), selected.bottomRow(), 1)]
-        #
-        # self.data = pd.DataFrame(book)  # переводим book в dataframe
-        # self.MainMenu.drawScreen.axes.cla()  # очищаем холст
-        #
-        # self.data.plot(ax=self.MainMenu.drawScreen.axes)  # рисуем график
-        # self.MainMenu.drawScreen.draw()  # обновляем холст
+        for selected in self.tableWidget.selectedRanges():  # бежим по выбранным значениям и добавляем их в book
+            for index in range(selected.leftColumn(), selected.rightColumn() + 1, 1):
+                book[headers[index]] = [values[j][index] for j in range(selected.topRow(), selected.bottomRow(), 1)]
+
+        self.data = pd.DataFrame(book)  # переводим book в dataframe
+        self.MainMenu.drawScreen.axes.cla()  # очищаем холст
+
+        self.data.plot(ax=self.MainMenu.drawScreen.axes)  # рисуем график
+        self.MainMenu.drawScreen.draw()  # обновляем холст
 
     def update(self):  # функция обновления окна
-        self.LineEdit.setText(self.MainMenu.delimiter)  # устанавливаем в строку текущий разделитель
+        # устанавливаем в строку текущий разделитель
         if self.data.empty:  # если данных нет, то заканчиваем функцию
             return
         self.tableWidget.setColumnCount(len(self.data.columns))  # задаем количество столбцов и строк
@@ -61,12 +59,44 @@ class SettingsWindow(QMainWindow, Ui_settings):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(it)))
 
 
+class SettingsWindow(QMainWindow, Ui_settings):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.setMinimumSize(500, 400)
+
+
+        self.pushButton_3.clicked.connect(self.apply)
+        self.pushButton_2.clicked.connect(self.close)
+        self.pushButton.clicked.connect(self.open)
+
+        self.data = None
+        self.MainMenu = None
+        self.DataMenu = None
+
+    def close_window(self):
+        self.close()
+
+    def apply(self):
+        pass
+        if self.lineEdit.text():  # если есть разделитель, то считываем и обновляем его
+            self.MainMenu.delimiter = self.lineEdit.text()
+    def open(self):
+        if self.DataMenu is None:
+            self.DataMenu = DataWinwow()  # создаем объект класса меню настроек
+        self.DataMenu.SettingsMenu = self
+        self.DataMenu.data = self.data  # передаем ему данные и объект класса основного окна
+        self.DataMenu.update()
+        self.DataMenu.show()
+
+
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         self.setMinimumSize(500, 400)
-        self.w = None  # создаем переменную для окна настройки
+        self.SettingWindow = None  # создаем переменную для окна настройки
         self.delimiter = ','  # переменная для хранения разделителя
         self.data = pd.DataFrame({})  # переменная для dataframe
 
@@ -86,7 +116,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             file_dialog = QFileDialog()
             file_path, _ = file_dialog.getOpenFileName(None, "Открыть файл")
             self.data = pd.read_csv(file_path, delimiter=self.delimiter)
-            self.update()
+            # self.update()
+            self.settings()
         except Exception:
             pass
 
@@ -98,12 +129,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.drawScreen.draw()
 
     def settings(self):  # функция вызова меню настроек
-        if self.w is None:
-            self.w = SettingsWindow()  # создаем объект класса меню настроек
-        self.w.MainMenu = self
-        self.w.data = self.data  # передаем ему данные и объект класса основного окна
-        self.w.update()
-        self.w.show()
+        if self.SettingWindow is None:
+            self.SettingWindow = SettingsWindow()  # создаем объект класса меню настроек
+        self.SettingWindow.MainMenu = self
+        self.SettingWindow.data = self.data  # передаем ему данные и объект класса основного окна
+        self.SettingWindow.update()
+        self.SettingWindow.show()
 
 
 if __name__ == "__main__":
